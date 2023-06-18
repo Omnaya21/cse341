88bb83265745aec2cd5a7f547b4f8c87cfc16450
Omar Navarro <omar.navarroy@yahoo.com>
2023-06-17 19:52:18 -0700

    Fixed punctuation error

--- L04/personal-assignment/index.js
+++ L04/personal-assignment/index.js
@@ -16,7 +16,7 @@ app
       'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST. PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
