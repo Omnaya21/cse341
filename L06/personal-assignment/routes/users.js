0244d463c43e4a07756d14cd599bf2d7869536bb
Omar Navarro <onavarro@ingyso.com>
2023-06-24 23:47:32 -0700

    Added id validation to users

--- L06/personal-assignment/routes/users.js
+++ L06/personal-assignment/routes/users.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', usersController.getAllUsers);
 router.get('/:id', idValidationRules(), validateCollection, usersController.getUser);
 router.post('/', userValidationRules(), validateCollection, usersController.newUser);
-router.put('/:id', userValidationRules(), validateCollection, usersController.editUser);
+router.put('/:id', idValidationRules(), userValidationRules(), validateCollection, usersController.editUser);
 router.delete('/:id', idValidationRules(), validateCollection, usersController.deleteUser);
 
 module.exports = router;
