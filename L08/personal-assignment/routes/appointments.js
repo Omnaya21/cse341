734abe1c4ec18464f0442dba208e513bf999827d
Omar Navarro <onavarro@ingyso.com>
2023-07-06 04:05:19 -0700

    Copied initial files for L08

---
+++ L08/personal-assignment/routes/appointments.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const appointmentstsController = require('../controllers/appointments');
+const { appointmentValidationRules, validateCollection, idValidationRules } = require('../validation');
+
+const router = express.Router();
+
+router.get('/', appointmentstsController.getAllAppointments);
+router.get('/:id', idValidationRules(), validateCollection, appointmentstsController.getAppointment);
+router.post('/', appointmentValidationRules(), validateCollection, appointmentstsController.newAppointment);
+router.put('/:id',idValidationRules(), appointmentValidationRules(), validateCollection, appointmentstsController.editAppointment);
+router.delete('/:id', idValidationRules(), validateCollection, appointmentstsController.deleteAppointment);
+router.delete('/', appointmentstsController.deleteAllAppointments);
+
+module.exports = router;
