d2dac1300e71bd60c680aba7fb0c0f715d53c8dd
Omar Navarro <onavarro@ingyso.com>
2023-07-07 17:57:27 -0700

    Added secure routes when accesing collections

--- L08/personal-assignment/routes/appointments.js
+++ L08/personal-assignment/routes/appointments.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const {auth, requiresAuth} = require('express-openid-connect');
 
 const appointmentstsController = require('../controllers/appointments');
 const { appointmentValidationRules, validateCollection, idValidationRules } = require('../validation');
 
 const router = express.Router();
 
-router.get('/', appointmentstsController.getAllAppointments);
-router.get('/:id', idValidationRules(), validateCollection, appointmentstsController.getAppointment);
-router.post('/', appointmentValidationRules(), validateCollection, appointmentstsController.newAppointment);
-router.put('/:id',idValidationRules(), appointmentValidationRules(), validateCollection, appointmentstsController.editAppointment);
-router.delete('/:id', idValidationRules(), validateCollection, appointmentstsController.deleteAppointment);
-router.delete('/', appointmentstsController.deleteAllAppointments);
+router.get('/', requiresAuth(), appointmentstsController.getAllAppointments);
+router.get('/:id', requiresAuth(), idValidationRules(), validateCollection, appointmentstsController.getAppointment);
+router.post('/', requiresAuth(), appointmentValidationRules(), validateCollection, appointmentstsController.newAppointment);
+router.put('/:id', requiresAuth(), idValidationRules(), appointmentValidationRules(), validateCollection, appointmentstsController.editAppointment);
+router.delete('/:id', requiresAuth(), idValidationRules(), validateCollection, appointmentstsController.deleteAppointment);
+router.delete('/', requiresAuth(), appointmentstsController.deleteAllAppointments);
 
 module.exports = router;
--- L08/personal-assignment/routes/users.js
+++ L08/personal-assignment/routes/users.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const {requiresAuth} = require('express-openid-connect'); 
 
 const usersController = require('../controllers/users');
 const { userValidationRules, validateCollection, idValidationRules } = require('../validation');
 
 const router = express.Router();
 
-router.get('/', usersController.getAllUsers);
-router.get('/:id', idValidationRules(), validateCollection, usersController.getUser);
-router.post('/', userValidationRules(), validateCollection, usersController.newUser);
-router.put('/:id', idValidationRules(), userValidationRules(), validateCollection, usersController.editUser);
-router.delete('/:id', idValidationRules(), validateCollection, usersController.deleteUser);
+router.get('/', requiresAuth(), usersController.getAllUsers);
+router.get('/:id', requiresAuth(), idValidationRules(), validateCollection, usersController.getUser);
+router.post('/', requiresAuth(), userValidationRules(), validateCollection, usersController.newUser);
+router.put('/:id', requiresAuth(), idValidationRules(), userValidationRules(), validateCollection, usersController.editUser);
+router.delete('/:id', requiresAuth(), idValidationRules(), validateCollection, usersController.deleteUser);
 
 module.exports = router;
